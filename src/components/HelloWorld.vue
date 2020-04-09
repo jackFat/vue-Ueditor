b1cfd461d6e68e1a2cac33436994e7e641167f26
jack_fat <chenhao@weizaojiao.cn>
2020-04-09 10:05:15 +0800

    init ue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="container">
+    <Uediter :value="ueditor.value" :config="ueditor.config" ref="ue"></Uediter>
+
+    <input type="button" value="显示编辑器内容（从控制台查看）" @click="returnContent" />
   </div>
 </template>
 
 <script>
+import Uediter from "@/components/UEditor.vue";
+
 export default {
-  name: 'HelloWorld',
-  data () {
+  components: { Uediter },
+
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      dat: {
+        content: ""
+      },
+
+      ueditor: {
+        value: "编辑器默认文字",
+
+        config: {}
+      }
+    };
+  },
+
+  methods: {
+    returnContent() {
+      this.dat.content = this.$refs.ue.getUEContent();
+
+      console.log(this.dat.content);
     }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.container {
+  padding: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import '../static/ue/ueditor.config.js'
+
+import '../static/ue/ueditor.all.min.js'
+
+import '../static/ue/lang/zh-cn/zh-cn.js'
+
+import '../static/ue/ueditor.parse.min.js'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
